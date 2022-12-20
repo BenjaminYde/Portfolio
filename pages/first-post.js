17835f177572a9be64763579f1cb499d3fa5c83d
Benjamin Yde <benjamin.yde@gmail.com>
2022-12-20 01:06:52 +0100

    github actions: try to deploy

--- pages/first-post.js
+++ pages/first-post.js
@@ -24,23 +24,25 @@ export default function Page({ allPostsData }) {
                 <link rel="icon" href="/images/profile.jpg" />
             </Head>
 
-            <Script
+{/*            <Script
                 src="https://connect.facebook.net/en_US/sdk.js"
                 strategy="lazyOnload"
                 onLoad={() =>
                     console.log(`script loaded correctly, window.FB has been populated`)
                 }
-            />
+            />*/}
 
             <h1>First Post</h1>
             <h2>
                 <Link href="/">Back to home</Link>
             </h2>
 
-            <img src="/images/profile.jpg" // Route of the image file
-                 height={256} // Desired size with correct aspect ratio
-                 width={256} // Desired size with correct aspect ratio
-                 alt="Your Name" />
+            <Image
+                src="/images/profile.jpg" // Route of the image file
+                height={256} // Desired size with correct aspect ratio
+                width={256} // Desired size with correct aspect ratio
+                alt="Your Name"
+            />
 
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
