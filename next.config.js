ad7e6240a3adc23c415bd3b30731956544a7bb14
Benjamin Yde <benjamin.yde@gmail.com>
2022-12-20 02:20:02 +0100

    github actions: try to deploy

--- next.config.js
+++ next.config.js
@@ -1,34 +1,8 @@
-// next.config.js
+const debug = process.env.NODE_ENV !== 'production';
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = '/'
-let basePath = ''
-
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-    assetPrefix = `/${repo}/`
-    basePath = `/${repo}`
-}
-
-/*
-const withNextOptimizedImages = require('next-optimized-images');
-
-module.exports = withNextOptimizedImages(
-    {
-        assetPrefix: assetPrefix,
-        basePath: basePath,
-        handleImages: ['jpeg', 'png', 'gif'],
-    });
-*/
-
-
-const withOptimizedImages = require('next-optimized-images');
-
-module.exports = withOptimizedImages({
-    assetPrefix: assetPrefix,
-    basePath: basePath,
-    handleImages: ['jpeg', 'png', 'gif'],
-    optimizeImagesInDev: true
-});
+module.exports = {
+    assetPrefix: !debug ? process.env.GITHUB_REPOSITORY : '',
+    images: {
+        unoptimized: true,
+    },
+};
