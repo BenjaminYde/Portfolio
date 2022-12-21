cfc5dc619073eb33a1ef3d30c846f250e2d7f9c0
Benjamin Yde <benjamin.yde@gmail.com>
2022-12-21 20:43:18 +0100

    github pages: try to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig   = {
     images: {
         loader: 'akamai',
         path: '',
     },
-    assetPrefix: './portfolio',
+    assetPrefix: isProd ? 'https://benjaminyde.github.io/portfolio/' : './',
+    basePath: isProd ? '/portfolio' : './',
 };
 
 export default   nextConfig;
