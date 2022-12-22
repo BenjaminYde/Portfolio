f53ea9187d61c447ae4ca6355453761d4d57c2f9
Benjamin Yde <benjamin.yde@gmail.com>
2022-12-22 03:21:30 +0100

    removed non-needed files

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.scss";
+import "../public/fonts/global_fonts.scss";
 import Head from "next/head";
 import Navigation from "../components/navigation";
 import Footer from "../components/footer";
@@ -7,7 +8,7 @@ export default function App({Component, pageProps}) {
     return (
         <>
             <Head>
-                <link rel="icon" href="./images/icons/tab_icon.ico"/>
+                <link rel="icon" href="/images/icons/tab_icon.ico"/>
             </Head>
             <Navigation/>
             <Component {...pageProps} />
